61707509382f49f7d38aacc8f95d094643d2b509
Glauber Silva <glauber.lucio.silva@gmail.com>
2020-09-29 01:13:06 -0300

    add rotate_char functions

--- lib/rotational_cipher.ex
+++ lib/rotational_cipher.ex
@@ -6,15 +6,23 @@ defmodule RotationalCipher do
   iex> RotationalCipher.rotate("Attack at dawn", 13)
   "Nggnpx ng qnja"
   """
-  @upper
 
   @spec rotate(text :: String.t(), shift :: integer) :: String.t()
   def rotate(text, shift) do
     text
-    |> String.graphemes()
-    |> rotate_char(shift)
+    |> String.to_charlist()
+    |> Enum.map_join(&rotate_char(&1, shift))
   end
 
-  defp rotate_char(x, shift) when ?x in 97..122, do: <<char>>
-  defp rotate_char(x, shift) when ?x in 65..90, do: <<char>>
+  defp rotate_char(char, shift) when char >= ?a and char <= ?z do
+    <<rem(char - ?a + shift, 26) + ?a>>
+  end
+
+  defp rotate_char(char, shift) when char >= ?A and char <= ?Z do
+    <<rem(char - ?A + shift, 26) + ?A>>
+  end
+
+  defp rotate_char(char, _) do
+    <<char>>
+  end
 end
